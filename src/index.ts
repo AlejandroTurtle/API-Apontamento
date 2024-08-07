ed1635fbaf7b0aa3271eac4846b8e51c7b3770d1
Alejandro <alejandrogomes23@hotmail.com>
2024-08-07 19:33:32 -0300

    a

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import routeApontamento from './routes/apontamento'
 import bodyParser from "body-parser"
 import cors from 'cors'
 
+
 const app = express()
+
 
 const port = process.env.PORT
 
@@ -12,20 +14,9 @@ const port = process.env.PORT
 
 app.use(bodyParser.json())
 app.use(cors())
-app.use("/", authroutes, routeApontamento)
+app.use("/api", authroutes, routeApontamento)
 
-const startServer = async () => {
-    try {
-      app.listen(port, () => {
-        console.log(`Servidor rodando na porta ${port}`)
-      })
-    } catch (error) {
-      console.error('Erro ao iniciar o servidor:', error)
-    }
-  }
-  
-  startServer()
-
+app.listen(port, () => console.log(`Server running on port ${port}`))
 
 
 
