7f8cb6135685372a31e003b1efae11eeb5745961
Alejandro <alejandrogomes23@hotmail.com>
2024-08-07 20:09:42 -0300

    teste dnv

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,16 @@
-import express, { Request, Response } from "express"
-import authroutes from './routes/auth'
-import routeApontamento from './routes/apontamento'
-import bodyParser from "body-parser"
-import cors from 'cors'
-
+import express, { Request, Response } from 'express'
 
 const app = express()
-
-
-const port = process.env.PORT
-
-
-
-app.use(bodyParser.json())
-app.use(cors())
+const port = process.env.PORT || 8080
 
 app.get('/', (_req: Request, res: Response) => {
-    return res.send('Express Typescript on Vercel')
-  })
+  return res.send('Express Typescript on Vercel')
+})
 
-  app.get('/ping', (_req: Request, res: Response) => {
-    return res.send('pong 🏓')
-  })
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓')
+})
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
-
-
-
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`)
+})
