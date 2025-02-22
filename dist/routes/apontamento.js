69ce2f5362ec97b7042b06bcdede276b763de90a
Alejandro <alejandro@jrmendonca.com.br>
2025-02-21 23:23:55 -0300

    implementado envio de mensagem pro portifolio

--- dist/routes/apontamento.js
+++ dist/routes/apontamento.js
@@ -15,11 +15,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const apontamento_model_1 = __importDefault(require("../models/apontamento.model"));
 const authenticateToken_1 = __importDefault(require("../middleware/authenticateToken"));
+const nodemailer_1 = __importDefault(require("nodemailer"));
 const routeApontamento = express_1.default.Router();
+const smtp = nodemailer_1.default.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "suportealejandrog@gmail.com",
+        pass: "gsgt rmma dxgh huzk",
+    },
+    tls: {
+        rejectUnauthorized: true,
+    },
+});
 routeApontamento.post("/apontamento", authenticateToken_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { data, entrada, saida, atividade } = req.body;
     try {
-        const apontamentoExistente = yield apontamento_model_1.default.findOne({ where: { userId: req.userId, data, entrada } });
+        const apontamentoExistente = yield apontamento_model_1.default.findOne({
+            where: { userId: req.userId, data, entrada },
+        });
         if (apontamentoExistente) {
             return res.status(400).json({ message: "Horário já apontado" });
         }
@@ -33,8 +48,8 @@ routeApontamento.post("/apontamento", authenticateToken_1.default, (req, res) =>
         res.status(201).json(apontamento);
     }
     catch (error) {
-        console.error('Erro ao criar apontamento:', error);
-        res.status(500).json({ message: 'Erro ao criar apontamento', error });
+        console.error("Erro ao criar apontamento:", error);
+        res.status(500).json({ message: "Erro ao criar apontamento", error });
     }
 }));
 routeApontamento.get("/apontamento", authenticateToken_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -45,8 +60,8 @@ routeApontamento.get("/apontamento", authenticateToken_1.default, (req, res) => 
         res.status(200).json(apontamentos);
     }
     catch (error) {
-        console.error('Erro ao buscar apontamentos:', error);
-        res.status(500).json({ message: 'Erro ao buscar apontamentos', error });
+        console.error("Erro ao buscar apontamentos:", error);
+        res.status(500).json({ message: "Erro ao buscar apontamentos", error });
     }
 }));
 routeApontamento.put("/apontamento/:id", authenticateToken_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -55,22 +70,22 @@ routeApontamento.put("/apontamento/:id", authenticateToken_1.default, (req, res)
     try {
         const apontamento = yield apontamento_model_1.default.findByPk(id);
         if (!apontamento) {
-            return res.status(404).json({ message: 'Apontamento não encontrado' });
+            return res.status(404).json({ message: "Apontamento não encontrado" });
         }
         if (apontamento.userId !== req.userId) {
-            return res.status(403).json({ message: 'Permissão negada' });
+            return res.status(403).json({ message: "Permissão negada" });
         }
         yield apontamento.update({
             data,
             entrada,
             saida,
-            atividade
+            atividade,
         });
-        res.status(200).json({ message: 'Apontamento atualizado com sucesso' });
+        res.status(200).json({ message: "Apontamento atualizado com sucesso" });
     }
     catch (error) {
-        console.error('Erro ao atualizar apontamento:', error);
-        res.status(500).json({ message: 'Erro ao atualizar apontamento', error });
+        console.error("Erro ao atualizar apontamento:", error);
+        res.status(500).json({ message: "Erro ao atualizar apontamento", error });
     }
 }));
 routeApontamento.delete("/apontamento/:id", authenticateToken_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -78,18 +93,43 @@ routeApontamento.delete("/apontamento/:id", authenticateToken_1.default, (req, r
     try {
         const apontamento = yield apontamento_model_1.default.findByPk(id);
         if (!apontamento) {
-            return res.status(404).json({ message: 'Apontamento não encontrado' });
+            return res.status(404).json({ message: "Apontamento não encontrado" });
         }
         if (apontamento.userId !== req.userId) {
-            return res.status(403).json({ message: 'Permissão negada' });
+            return res.status(403).json({ message: "Permissão negada" });
         }
         yield apontamento.destroy();
-        res.status(200).json({ message: 'Apontamento excluído com sucesso' });
+        res.status(200).json({ message: "Apontamento excluído com sucesso" });
     }
     catch (error) {
-        console.error('Erro ao excluir apontamento:', error);
-        res.status(500).json({ message: 'Erro ao excluir apontamento', error });
+        console.error("Erro ao excluir apontamento:", error);
+        res.status(500).json({ message: "Erro ao excluir apontamento", error });
     }
+}));
+routeApontamento.post("/apontamento/entraremContato", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { nome, email, mensagem } = req.body;
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Mensagem:", mensagem);
+    let message = {
+        from: "suportealejandrog@gmail.com",
+        to: "alejandrogomes23@hotmail.com",
+        subject: "Nova mensagem do seu portfólio",
+        html: `
+        <p>Olá Alejandro,</p>
+        <p>Você recebeu uma nova mensagem do seu portfólio:</p>
+        <p><strong>Nome:</strong> ${nome}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Mensagem:</strong></p>
+        <p>${mensagem}</p>
+        <p>Atenciosamente,</p>
+        <p>Sua equipe de suporte</p>
+      `,
+    };
+    console.log("Enviando e-mail");
+    yield smtp.sendMail(message);
+    console.log("Email enviado");
+    res.status(201).json({ message: "Mensagem enviada com sucesso" });
 }));
 exports.default = routeApontamento;
 //# sourceMappingURL=apontamento.js.map
--- src/routes/apontamento.ts
+++ src/routes/apontamento.ts
@@ -1,96 +1,152 @@
-import express, { Request, Response } from 'express'
-import Apontamento  from '../models/apontamento.model'
-import authenticateToken from '../middleware/authenticateToken'
+import express, { Request, Response } from "express";
+import Apontamento from "../models/apontamento.model";
+import authenticateToken from "../middleware/authenticateToken";
+import nodemailer from "nodemailer";
 
+const routeApontamento = express.Router();
 
-const routeApontamento = express.Router()
+declare module "express-serve-static-core" {
+  interface Request {
+    userId?: number;
+  }
+}
 
-declare module 'express-serve-static-core' {
-    interface Request {
-      userId?: number
+const smtp = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "suportealejandrog@gmail.com",
+    pass: "gsgt rmma dxgh huzk",
+  },
+  tls: {
+    rejectUnauthorized: true,
+  },
+});
+
+routeApontamento.post("/apontamento", authenticateToken, async (req, res) => {
+  const { data, entrada, saida, atividade } = req.body;
+
+  try {
+    const apontamentoExistente = await Apontamento.findOne({
+      where: { userId: req.userId, data, entrada },
+    });
+
+    if (apontamentoExistente) {
+      return res.status(400).json({ message: "Horário já apontado" });
+    }
+
+    const apontamento = await Apontamento.create({
+      userId: req.userId,
+      data,
+      entrada,
+      saida,
+      atividade,
+    });
+
+    res.status(201).json(apontamento);
+  } catch (error) {
+    console.error("Erro ao criar apontamento:", error);
+    res.status(500).json({ message: "Erro ao criar apontamento", error });
+  }
+});
+
+routeApontamento.get(
+  "/apontamento",
+  authenticateToken,
+  async (req: Request, res: Response) => {
+    try {
+      const apontamentos = await Apontamento.findAll({
+        where: { userId: req.userId },
+      });
+      res.status(200).json(apontamentos);
+    } catch (error) {
+      console.error("Erro ao buscar apontamentos:", error);
+      res.status(500).json({ message: "Erro ao buscar apontamentos", error });
     }
   }
+);
 
-
-  routeApontamento.post("/apontamento", authenticateToken, async (req, res) => {
+routeApontamento.put(
+  "/apontamento/:id",
+  authenticateToken,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
     const { data, entrada, saida, atividade } = req.body;
-
     try {
-        const apontamentoExistente = await Apontamento.findOne({ where: { userId: req.userId, data, entrada } });
-
-        if (apontamentoExistente) {
-            return res.status(400).json({ message: "Horário já apontado" });
-        }
-
-        const apontamento = await Apontamento.create({
-            userId: req.userId,
-            data,
-            entrada,
-            saida,
-            atividade,
-        });
-
-        res.status(201).json(apontamento);
+      const apontamento = await Apontamento.findByPk(id);
+      if (!apontamento) {
+        return res.status(404).json({ message: "Apontamento não encontrado" });
+      }
+      if (apontamento.userId !== req.userId) {
+        return res.status(403).json({ message: "Permissão negada" });
+      }
+      await apontamento.update({
+        data,
+        entrada,
+        saida,
+        atividade,
+      });
+      res.status(200).json({ message: "Apontamento atualizado com sucesso" });
     } catch (error) {
-        console.error('Erro ao criar apontamento:', error);
-        res.status(500).json({ message: 'Erro ao criar apontamento', error });
+      console.error("Erro ao atualizar apontamento:", error);
+      res.status(500).json({ message: "Erro ao atualizar apontamento", error });
     }
-});
-    
-    routeApontamento.get("/apontamento", authenticateToken, async (req: Request, res: Response) => {
-      try {
-        const apontamentos = await Apontamento.findAll({
-          where: { userId: req.userId },
-        })
-        res.status(200).json(apontamentos)
-      } catch (error) {
-        console.error('Erro ao buscar apontamentos:', error)
-        res.status(500).json({ message: 'Erro ao buscar apontamentos', error });
-      }
-    })
+  }
+);
 
-    routeApontamento.put("/apontamento/:id", authenticateToken, async (req: Request, res: Response) => {
-      const { id } = req.params;
-      const { data, entrada, saida, atividade } = req.body;
-      try {
-          const apontamento = await Apontamento.findByPk(id);
-          if (!apontamento) {
-              return res.status(404).json({ message: 'Apontamento não encontrado' });
-          }
-          if (apontamento.userId !== req.userId) {
-              return res.status(403).json({ message: 'Permissão negada' });
-          }
-          await apontamento.update({
-              data,
-              entrada,
-              saida,
-              atividade
-          });
-          res.status(200).json({ message: 'Apontamento atualizado com sucesso' });
-      } catch (error) {
-          console.error('Erro ao atualizar apontamento:', error);
-          res.status(500).json({ message: 'Erro ao atualizar apontamento', error });
-      }
-  });
-
-  routeApontamento.delete("/apontamento/:id", authenticateToken, async (req: Request, res: Response) => {
+routeApontamento.delete(
+  "/apontamento/:id",
+  authenticateToken,
+  async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
-        const apontamento = await Apontamento.findByPk(id);
-        if (!apontamento) {
-            return res.status(404).json({ message: 'Apontamento não encontrado' });
-        }
-        if (apontamento.userId !== req.userId) {
-            return res.status(403).json({ message: 'Permissão negada' });
-        }
-        await apontamento.destroy();
-        res.status(200).json({ message: 'Apontamento excluído com sucesso' });
+      const apontamento = await Apontamento.findByPk(id);
+      if (!apontamento) {
+        return res.status(404).json({ message: "Apontamento não encontrado" });
+      }
+      if (apontamento.userId !== req.userId) {
+        return res.status(403).json({ message: "Permissão negada" });
+      }
+      await apontamento.destroy();
+      res.status(200).json({ message: "Apontamento excluído com sucesso" });
     } catch (error) {
-        console.error('Erro ao excluir apontamento:', error);
-        res.status(500).json({ message: 'Erro ao excluir apontamento', error });
+      console.error("Erro ao excluir apontamento:", error);
+      res.status(500).json({ message: "Erro ao excluir apontamento", error });
     }
-});
+  }
+);
 
+routeApontamento.post(
+  "/apontamento/entraremContato",
+  async (req: Request, res: Response) => {
+    const { nome, email, mensagem } = req.body;
 
+    console.log("Nome:", nome);
+    console.log("Email:", email);
+    console.log("Mensagem:", mensagem);
 
-export default routeApontamento
+    let message = {
+      from: "suportealejandrog@gmail.com",
+      to: "alejandrogomes23@hotmail.com",
+      subject: "Nova mensagem do seu portfólio",
+      html: `
+        <p>Olá Alejandro,</p>
+        <p>Você recebeu uma nova mensagem do seu portfólio:</p>
+        <p><strong>Nome:</strong> ${nome}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Mensagem:</strong></p>
+        <p>${mensagem}</p>
+        <p>Atenciosamente,</p>
+        <p>Sua equipe de suporte</p>
+      `,
+    };
+    console.log("Enviando e-mail");
+    await smtp.sendMail(message);
+    console.log("Email enviado");
+
+    res.status(201).json({ message: "Mensagem enviada com sucesso" });
+  }
+);
+
+export default routeApontamento;
